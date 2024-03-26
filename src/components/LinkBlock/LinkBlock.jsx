b54ded7a7d83d500fc8f131f20db5cebd11da430
Alona Astasova <aastashovaa@gmail.com>
2024-03-26 18:01:12 +0100

    init: Add SecondTextBlock and LinkBlock

--- src/components/LinkBlock/LinkBlock.jsx
+++ src/components/LinkBlock/LinkBlock.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import s from './LinkBlock.module.css'
 
 export default function LinkBlock() {
   return (
-    <div>LinkBlock</div>
+    <div className={s.mainBlock}>
+      <div className={s.linkBlock}>
+        <a href="#" className={s.button}>Fashion</a>
+      </div>
+      <div className={s.linkBlock}>
+        <a href="#" className={s.button}>Models</a>
+      </div>
+      <div className={s.linkBlock}>
+        <a href="#" className={s.button}>Brands</a>
+      </div>
+    </div>
   )
 }
--- src/components/SecondTextBlock/SecondTextBlock.jsx
+++ src/components/SecondTextBlock/SecondTextBlock.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import s from './SecondTextBlock.module.css'
 
 export default function SecondTextBlock() {
   return (
-    <div>SecondTextBlock</div>
+    <div className={s.textBlock}>
+      <p className={s.text1}>Luxurious and exclusive photo and video productions</p>
+      <p className={s.text2}>For brands, fashion and models</p>
+    </div>
   )
 }
