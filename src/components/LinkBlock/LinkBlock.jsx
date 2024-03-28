5cd9c1439ccf07499d3c06d0450ac1e266dab469
Ilya Butorin <butorynmi@gmail.com>
2024-03-28 22:15:26 +0100

    feat: add styles

--- src/components/LinkBlock/LinkBlock.jsx
+++ src/components/LinkBlock/LinkBlock.jsx
@@ -5,13 +5,13 @@ export default function LinkBlock() {
   return (
     <div className={s.mainBlock}>
       <div className={s.linkBlock}>
-        <a href="#" className={s.button}>Fashion</a>
+        <a href="/" className={s.button}>Fashion</a>
       </div>
       <div className={s.linkBlock}>
-        <a href="#" className={s.button}>Models</a>
+        <a href="/" className={s.button}>Models</a>
       </div>
       <div className={s.linkBlock}>
-        <a href="#" className={s.button}>Brands</a>
+        <a href="/" className={s.button}>Brands</a>
       </div>
     </div>
   )
