1477508cbaf0ac6199826b75dd719b186d90e7c0
Alona Astasova <aastashovaa@gmail.com>
2024-03-28 23:37:08 +0100

    feat: Update BusinessCard, website color, icons. Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import s from './Footer.module.css'
 import BusinessCard from '../../widgets/BusinessCard/BusinessCard'
 
 export default function Footer() {
   return (
-    <div>
-      <BusinessCard />
+    <div className={s.footer}>
+      <div className={s.contactBlock}>
+        <BusinessCard />
+      </div>
+      <p className={s.footerCopyright}>&copy; 2024 Made by BUTOR GmbH</p>
     </div>
   )
 }
--- src/widgets/BusinessCard/BusinessCard.jsx
+++ src/widgets/BusinessCard/BusinessCard.jsx
@@ -8,19 +8,19 @@ export default function BusinessCard() {
   return (
     <div className={s.mainBlock}>
         <div className={s.firstBlock}>
-            <p>Our contacts</p>
-            <p>Ceyla</p>
+            <p className={s.firstText}>Our contacts</p>
+            <p className={s.secondText}>Ceyla</p>
         </div>
         <div className={s.secondBlock}>
-            <div>
+            <div className={s.contactInfo}>
                 <img src={instagramIcon} alt="instagramIcon" />
                 <p>ceyla_media</p>
             </div>
-            <div>
+            <div className={s.contactInfo}>
                 <img src={mailIcon} alt="mailIcon" />
                 <p>contact@ceylamedia.com</p>
             </div>
-            <div>
+            <div className={s.contactInfo}>
                 <img src={whatsappIcon} alt="whatsappIcon" />
                 <p>+37120000000</p>
             </div>
